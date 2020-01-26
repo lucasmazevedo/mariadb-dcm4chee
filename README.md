5960bd4199a71b8df6029f234da2cc56717ddc65
Lucas Azevedo <lucasazevedo.dev@gmail.com>
2020-01-26 02:46:45 -0300

    mariadb-dcm4chee

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Docker image that provides a MariaDB instance for DCM4CHEE 5.x
 Based on https://github.com/dcm4che-dockerfiles/postgres-dcm4chee
 
 BETA VERSION
+
+# How to Use
+
+docker run --name mariadb \
+           -p 3306:3306 \
+           -v /etc/localtime:/etc/localtime \
+           -e MYSQL_DATABASE=pacsdb \
+           -e MYSQL_USER=pacs\
+           -e MYSQL_ROOT_PASSWORD=pacs \
+           -d lucasmazevedo/mariadb-dcm4chee
--- docker-entrypoint-initdb.d/00_init.sh
+++ docker-entrypoint-initdb.d/00_init.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Execute custom actions on startup. The script name starts with "00",
-# so it gets executed before the *.sql files defining the schema.
+# # Execute custom actions on startup. The script name starts with "00",
+# # so it gets executed before the *.sql files defining the schema.
 
-# Allow to define a custom name for the PACS database when starting a
-# container using MYSQL_DATABASE environmental variable
-cd /docker-entrypoint-initdb.d/ && for file in *.sql; do
-    echo "use $MYSQL_DATABASE;" > tmp.sql
-    cat $file >> tmp.sql
-    rm $file
-    mv tmp.sql $file
-done
+# # Allow to define a custom name for the PACS database when starting a
+# # container using MYSQL_DATABASE environmental variable
+# cd /docker-entrypoint-initdb.d/ && for file in *.sql; do
+#     echo "use $MYSQL_DATABASE;" > tmp.sql
+#     cat $file >> tmp.sql
+#     rm $file
+#     mv tmp.sql $file
+# done
